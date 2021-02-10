af7139e72857be7cbf8f90ea4595327ad457dc5b
alexbleggi <alexbleggi@gmail.com>
2021-02-10 20:36:21 -0300

    Ajustes na documentação

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,13 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   options: {
     storySort: {
-      order: ['General', ['Introdução', 'Utilização'], 'Tokens', ['Introdução', 'Cores', 'Tipografia', 'Espacamento', 'Complementares', 'Exemplo'], 'Global', 'Medsoft'],
+      order: [
+        'General', ['Introdução', 'Biblioteca', 'Padrões'],
+        'Tokens', ['Introdução', 'Cores', 'Tipografia', 'Espacamento', 'Complementares', 'Exemplo'],
+        'Global', ['Variables'],
+        'Medsoft', ['Arquitetura', 'Variables', 'Mixins'],
+        'External Pages',
+        'Inscrições'],
     },
   },
 }
--- README.md
+++ README.md
@@ -1,19 +1,5 @@
 # med-components
 
-## Instalação
-
-```bash
-npm install
-```
-
 ## Documentação
 
 - **[med-components](http://desenv.ordomederi.com/med-components)**
-
-## Utilização
-
-Para utilizar a biblioteca completa basta importar `med-components.scss`:
-
-```scss
-@import "~@mbrandao/med-components/src/med-components.scss";
-```
