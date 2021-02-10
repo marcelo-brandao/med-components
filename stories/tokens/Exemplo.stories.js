51b57902255f7322e5554e29a37b7e556539d3f3
alexbleggi <alexbleggi@gmail.com>
2021-02-10 15:41:14 -0300

    Ajustes documentação

--- stories/tokens/Exemplo.stories.js
+++ stories/tokens/Exemplo.stories.js
@@ -5,12 +5,12 @@ export default {
   decorators: [withDesign],
 };
 
-const TemplateTipografia = () => {
+const TemplateExemplo = () => {
   return `<h1 style="font-family: sans-serif; text-align: center;">Para visualizar clique na aba <span style="color: #3a8bd8;">Design</span> abaixo!</h1>`;
 };
 
-export const Tipografia = TemplateTipografia.bind({});
-Tipografia.parameters = {
+export const Exemplo = TemplateExemplo.bind({});
+Exemplo.parameters = {
   design: {
     type: 'figma',
     url: 'https://www.figma.com/file/4QvnRfhttlJ2hIKQbuEPZl/Tokens?node-id=519%3A0',
